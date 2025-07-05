17d4ecfb022e74381f98d7bd7b45e30ac9741e96
Ali Sena Danishwer <danishweralisena@gmail.com>
2025-07-06 00:11:40 +0800

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,4 +1,10 @@
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, EXTRA
+USE alx_book_store;
+
+SELECT COLUMN_NAME AS 'Column',
+       COLUMN_TYPE AS 'Type',
+       IS_NULLABLE AS 'Nullable',
+       COLUMN_KEY AS 'Key',
+       EXTRA AS 'Extra'
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = 'alx_book_store'
-AND TABLE_NAME = 'books';
+  AND TABLE_NAME = 'books';
